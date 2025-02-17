f22233d9494ced9dc479166dfccc66c951595f55
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-02-16 22:56:55 -0600

    just-practice

--- 02-variables.sh
+++ 02-variables.sh
@@ -13,5 +13,6 @@ echo "$PERSON2:: Hi, $PERSON1, HOw you doing?"
 
 PERSON1=$1
 PERSON2=$2
+
 echo "$PERSON1:: Hello $PERSON2"
 echo "$PERSON2:: Hi, $PERSON1, HOw you doing?"
--- 03-variablearguments.sh
+++ 03-variablearguments.sh
@@ -4,6 +4,4 @@ echo "Please enter username"
 read  USERNAME
 echo "Please enter password"
 read  PASSWORD
-
-echo "username is:: $USERNAME", password is :: $PASSWORD"
-
+echo "username is:: $USERNAME, password is :: $PASSWORD"
