172f04b4d2e1bb70cdef87c9471e63b03bce5cb3
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-02-16 23:06:45 -0600

    just-practice

--- 04-variablehid.sh
+++ 04-variablehid.sh
@@ -4,6 +4,4 @@ echo "Please enter username"
 read -s USERNAME
 echo "Please enter password"
 read -s PASSWORD
-
-echo "username is:: $USERNAME", password is :: $PASSWORD"
-
+echo "username is :: $USERNAME, password is :: $PASSWORD"
