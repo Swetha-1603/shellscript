06c3d232584be35d940cd4b52ea4353e475c5553
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-02-17 14:12:14 -0600

    just-practice

--- 01-variable.sh
+++ 01-variable.sh
@@ -6,4 +6,4 @@ PERSON2=Rahul
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
-echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
