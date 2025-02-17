449ec3c17b462016580648d1d384bfeeaea4bc01
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-02-16 22:47:27 -0600

    just-practice

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,3 +4,14 @@ echo "Ramesh:: Hello Suresh, How are you?"
 echo "Suresh:: Hi Ramesh, I am fine. How are you?"
 echo "Ramesh: I am fine too. how is your work?"
 echo "Suresh:: not bad. I am thinking to upgrade to DevOps"
+
+PERSON1=sachin
+PERSON2=mani
+
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hi, $PERSON1, HOw you doing?"
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hi, $PERSON1, HOw you doing?"
--- 04-varibleshiding.sh
+++ 04-varibleshiding.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter username"
-read  USERNAME
+read -s USERNAME
 echo "Please enter password"
-read  PASSWORD
+read -s PASSWORD
 
 echo "username is:: $USERNAME", password is :: $PASSWORD"
 
