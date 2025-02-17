a90ae784234ec3c2f3b834f2512428976bbbc3b6
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-02-17 13:58:29 -0600

    just-practice

--- 01-hello.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "My name is Swetha"
-echo "I am learning devops"
--- 04-variablehid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Please enter username"
-read -s USERNAME
-echo "Please enter password"
-read -s PASSWORD
-echo "username is :: $USERNAME, password is :: $PASSWORD"
