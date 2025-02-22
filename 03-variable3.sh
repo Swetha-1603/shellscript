17b950097d617e9c27466f0ad164f790ffde923d
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-02-22 12:42:15 -0600

    justpractice

--- 03-variable3.sh
+++ 03-variable3.sh
@@ -3,5 +3,5 @@
 echo "Please enter username::"
 read USERNAME
 echo "Please enter password::"
-read -s PASSWORD
+read -s PASSWORD  # this -s will hide the data entered while executing
 echo "username is :: $USERNAME, password is :: $PASSWORD"
