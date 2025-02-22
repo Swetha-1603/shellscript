e57c5eacc8064d93a1abd68468393e2254f30fec
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-02-22 14:22:17 -0600

    justpractice

---
+++ 10-validate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+VALIDATE(){
+if [$1 -ne 0]
+then 
+    echo "$2 is Failure"
+    exit 1
+else
+    echo "$2 is sucess"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run with root user"
+    exit 1
+else
+    echo "You are a root user"
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "installing MYSQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "installing Git"
