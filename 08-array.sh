6f96481c1b453e4af1f329e3a3ea7b5c1096c522
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-03-15 13:02:23 -0500

    justpractice

--- 08-array.sh
+++ 08-array.sh
@@ -3,4 +3,4 @@
 MOVIES=("RRR" "Tillu" "Murari")
 echo "First movie:: ${MOVIES[0]}"
 echo "Second movie :: ${MOVIES[1]}"
-echo "third movie :: ${MOVIES[@]}"
+echo "third movie :: ${MOVIES[@]}" # print all movies in the array
---
+++ 15-other.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="Decops from other script"
+
+echo "Varible value from other script : $COURSE"
+echo "Process ID of other script : $$"
