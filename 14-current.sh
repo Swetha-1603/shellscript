0465332fe6a55cc7c6b33a4f3a8e7c1493783f01
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-03-15 13:09:38 -0500

    justpractice

--- 14-current.sh
+++ 14-current.sh
@@ -4,6 +4,5 @@ COURSE="Devops from current script"
 
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
-./15-other.sh
-
+    ./15-other.sh
 echo "after calling other script, course: $COURSE"
