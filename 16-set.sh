cc6c6374941f03d679c1db5a9069e4074354fbf7
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-03-17 16:45:55 -0500

    justpractice

---
+++ 16-set.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+failure(){
+    echo "Filed at $1 : $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root access."
+    exit 1
+else 
+    echo "You are a super user."
+fi 
+
+dnf install mysqfff -y
+
+echo "is script proceeding"
