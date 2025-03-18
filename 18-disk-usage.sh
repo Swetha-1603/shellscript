2205649e6a737188d2fd5fb2ace45fc71062ec74
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-03-17 20:42:14 -0500

    justpractice

---
+++ 18-disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=6
+MESSAGE=""
+
+while IFS= read -r line
+DO  
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then 
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE\n"
+    fi
+done <<<$DISK_USAGE
+
+echo -e "Message : $MESSAGE"
+
+echo "$MESSAGE" | mail -s "Disk usage alert" @gmail.com
